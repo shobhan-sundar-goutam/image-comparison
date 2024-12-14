6c41630c9a609e9e25176fcb21935af8746b6539
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2024-12-14 11:20:00 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ app.use(urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://image-comparison-ssg.vercel.app/',
+    ],
   })
 );
 
